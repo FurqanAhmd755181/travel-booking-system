4cdc667e8033350ae651c591823d7cd187ca5d79
Furqan Ahmad <furqan.ahmad.ze@gmail.com>
2026-01-08 03:16:34 +0500

    small changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,15 @@
-// import Book from "./book/page";
-// import Services from "./servicesOffering/page";
-// import Cancel from "./cancel/page";
-import Availability from "./Availability/page";
+import Book from "./book/page";
+import Services from "./servicesOffering/page";
+import Cancel from "./cancel/page";
+// import Availability from "./Availability/page";
 
 export default function Home() {
   return (
     <main className="bg-gray-50 min-h-screen p-4 md:p-8">
-      {/* <Book />
+      <Book />
       <Services />
-      <Cancel /> */}
-      <Availability />
+      <Cancel />
+      {/* <Availability /> */}
 
     </main>
   );
